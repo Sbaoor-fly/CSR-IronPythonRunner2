d58b3f907d67589c870324e5ad2e6f6d4faa8f24
Sbaoor <70316422+Sbaoor-fly@users.noreply.github.com>
2021-02-22 19:38:19 +0800

    适配插件
    RT

---
+++ IPYRMods/EconomicAdmin/EconomicAdmin.net.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+import json
+import time
+import thread
+formid = {}
+page = {}
+toname = {}
+def econinputcommand(a):
+    d = mc.AnalysisEvent(a)
+    if d.cmd == '/econadmin' and IFOP(str(d.playername)):       
+        econSendForm(str(d.playername),tool.GetShareFunc('GetUUID')(str(d.playername)),'main')
+        return False
+
+def econformselect(a):
+    d = mc.AnalysisEvent(a)
+    if int(d.formid) == int(formid[d.playername]) and d.selected != 'null':
+        if page[d.playername] == 'main':
+            j = json.loads(mc.getOnLinePlayers())
+            pln = j[int(d.selected)]['playername']
+            toname[d.playername] = str(pln)
+            gui = mc.creatGUI('经济管理——'+str(pln))
+            gui.AddDropdown('模式',0,'[减少,增加]')
+            gui.AddInput('操作数值','数值')
+            formid[d.playername] = gui.SendToPlayer(d.uuid)
+            page[d.playername] = 'main2'
+        if page[d.playername] == 'main2':
+            jsons = eval('{\'selected\':'+str(d.selected)+'}')
+            if jsons['selected'][1].isdigit():
+                if jsons['selected'][0] == 0:
+                    if mc.getscoreboard(tool.GetShareFunc('GetUUID')(str(toname[d.playername])),'money') > int(jsons['selected'][1]):
+                        mc.runcmd('scoreboard players remove \"'+str(toname[d.playername])+'\" money '+str(jsons['selected'][1]))
+                        mc.sendText(d.uuid,'[§eEconCore§r] 已为'+str(toname[d.playername])+'移除'+str(jsons['selected'][1])+'金币！')
+                    else:
+                        mc.sendText(d.uuid,'[§eEconCore§r] '+str(toname[d.playername])+'的钱不够！')
+                if jsons['selected'][0] == 1:
+                    mc.runcmd('scoreboard players add \"'+str(toname[d.playername])+'\" money '+str(jsons['selected'][1]))
+                    mc.sendText(d.uuid,'[§eEconCore§r] 已为'+str(toname[d.playername])+'添加'+str(jsons['selected'][1])+'金币！')
+            else:
+                mc.sendText(d.uuid,'[§eEconCore§r] §4请输入正确格式的数字')
+        formid[d.playername] = '0'
+        page[d.playername] = '0'
+        toname[d.playername] = '0'
+
+def econSendForm(plname,uuid,pag):
+    a = json.loads(mc.getOnLinePlayers())
+    monlist = []
+    for key in a:
+        monlist.append(str(key['playername'])+'\n'+str(mc.getscoreboard(str(key['uuid']),'money'))+'$')
+    formid[plname] = mc.sendSimpleForm(uuid,'§3经济管理面板','',str(monlist).replace('\'','\"'))
+    page[plname] = pag
+    
+#########################
+def IFOP(plname):
+    a = json.loads(IO.File.ReadAllText('permissions.json'))
+    xuid = GetXUID(plname)
+    for key in a:
+        if key['xuid'] == xuid:
+            if key['permission'] == 'operator':
+                return True
+    return False
+def GetXUID(plname):
+    a = json.loads(mc.getOnLinePlayers())
+    for key in a:
+        if key['playername'] == plname:
+            return key['xuid']
+    return 'null'
+    
+ipyapi.Listen('onInputCommand',econinputcommand)
+ipyapi.Listen('onFormSelect',econformselect)
+mc.setCommandDescribe('econadmin','打开经济管理面板')
+print('[EconAdmin] 装载成功！')
+print('[EconAdmin] 作者：Sbaoor')
+print('[EconAdmin] 当前版本：1.2.0')
---
+++ IPYRMods/SaveBag/SaveBag.net.py
@@ -0,0 +1,96 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+# author : Sbaoor
+# date   : 2021/1/3
+import json
+import time
+import thread
+from System import IO
+formid = {}
+player = 0
+
+
+def svplayer_left(a):
+    global player
+    player -= 1
+
+def svsavepack():
+    global player
+    print '[SaveBag] 线程开启！'
+    while True:
+        if player > 0:
+            for x in json.loads(mc.getOnLinePlayers()):
+                i = mc.creatPlayerObject(x['uuid'])
+                packdict = json.loads(i.InventoryContainer)
+                xx = 0
+                pack = ''
+                while xx < int(len(packdict)):
+                    if not packdict[xx]["id"] == 0:
+                        pack += packdict[xx]["item"] + '@' + str(packdict[xx]["count"]) +'\n'
+                    xx += 1
+                tool.WriteAllText('./plugins/SaveBag/'+str(i.getName())+'.txt',pack)
+            print '[SaveBag] 数据保存完成,保存了'+str(player)+'个玩家的背包数据'
+        time.sleep(60)
+def svrespawn(a):
+    global player
+    formid[a.playername] = 0
+    if player == 0:
+        thread.start_new_thread(svsavepack,())
+        player += 1
+def svinputcommand(a):
+    if a.cmd == '/checkbag':
+        if IFOP(a.playername):
+            thread.start_new_thread(svsendonline,(a.playername,))
+        else:
+            mc.sendText(GetUUID(a.playername),'莫得权限，爬')
+        return False
+def svformselect(a):
+    if int(a.formid) == formid[a.playername]:
+        b = json.loads(mc.getOnLinePlayers())
+        i = mc.creatPlayerObject(GetUUID(str(b[int(a.selected)]['playername'])))
+        packdict = json.loads(i.InventoryContainer)
+        pack = '§3CheckBag§r >>> §4正在展示背包物品!§r [§2'+str(b[int(a.selected)]['playername'])+'§r]\n'
+        for xx in packdict:
+            if not xx["id"] == 0:
+                pack += xx["item"] + '@' + str(xx["count"]) +'\n'
+        mc.sendText(GetUUID(a.playername),str(pack))
+
+def GetUUID(plname):
+    a = json.loads(mc.getOnLinePlayers())
+    for key in a:
+        if key['playername'] == plname:
+            return key['uuid']
+    return 'null'
+
+def IFOP(plname):
+    a = json.loads(IO.File.ReadAllText('permissions.json'))
+    xuid = GetXUID(plname)
+    for key in a:
+        if key['xuid'] == xuid:
+            if key['permission'] == 'operator':
+                return True
+    return False
+
+def GetXUID(plname):
+    a = json.loads(mc.getOnLinePlayers())
+    for key in a:
+        if key['playername'] == plname:
+            return key['xuid']
+    return 'null'
+
+def svsendonline(name):
+    lists = []
+    a = json.loads(mc.getOnLinePlayers())
+    for key in a:
+        lists.append(key['playername'])
+    formid[name] = mc.sendSimpleForm(GetUUID(name),'查包面板','选择谁？',str(lists).replace('\'','\"'))
+ipyapi.lists('onFormSelect',svformselect)
+ipyapi.Listen('onInputCommand',svinputcommand)
+ipyapi.Listen('onPlayerLeft',svplayer_left)
+ipyapi.Listen('onLoadName',svrespawn)
+mc.setCommandDescribe('checkbag','管理员查包')
+if not tool.IfDir('./plugins/SaveBag'):
+    tool.CreateDir('./plugins/SaveBag')
+    print('[SaveBag] 文件夹创建成功！')
+print('[SaveBag] 背包保存已加载！')
+print('[SaveBag] 背包查询已加载！')
